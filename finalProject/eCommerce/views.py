c53cb1007a002b6e75c77324ddd61e35a590509f
Andabero5 <adbeltranr@correo.udistrital.edu.co>
2021-06-14 19:20:42 -0500

    homePage

--- finalProject/eCommerce/views.py
+++ finalProject/eCommerce/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+latestProducts = ["carro", "Moto", "Camion"]
+otherProducts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 
 def index(request):
-    return render(request, "a.html")
-
-
-def idex2(request, name):
-    return HttpResponse(name+"hola")
+    return render(request, "a.html", {
+        "latestProducts": latestProducts,
+        "otherProducts": otherProducts
+    })
